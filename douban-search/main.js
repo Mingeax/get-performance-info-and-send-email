e9aa03dfbae7865be7453b6e1c8059000d09b758
Mingeax <wsyym@qq.com>
2025-11-15 21:27:14 +0800

    feat: 电影检索后把数据输出到文件中.

--- douban-search/main.js
+++ douban-search/main.js
@@ -1,6 +1,20 @@
 import { JSDOM } from "jsdom";
+import { writeFile, mkdir, appendFile, rm } from "node:fs/promises";
+import { existsSync } from "node:fs";
+import { resolve } from "node:path";
 
 const baseUrl = "https://www.douban.com/doulist/111299960/";
+
+const outputFileName = {
+  bad: "bad_film",
+  good: "good_film",
+};
+const distDir = resolve(import.meta.dirname, "dist");
+
+const outputFilePath = {
+  bad: resolve(distDir, `${outputFileName.bad}.txt`),
+  good: resolve(distDir, `${outputFileName.good}.txt`),
+};
 
 const urlObj = new URL(baseUrl);
 
@@ -13,37 +27,53 @@ const html = await res.text();
 
 const dom = new JSDOM(html);
 
-dom.window.document.querySelectorAll(".doulist-item").forEach((listItem, i) => {
-  const itemBody = listItem.querySelector(".mod .bd.doulist-subject");
+rm(distDir, { force: true, recursive: true })
+  .finally(() => mkdir(distDir))
+  .then(() => {
+    return Promise.allSettled([
+      writeFile(outputFilePath.good, `查询时间: ${new Date()}\n`),
+      writeFile(outputFilePath.bad, `查询时间: ${new Date()}\n`),
+    ]);
+  })
+  .then(() => interpretDomNOutput());
 
-  const anchor = itemBody.querySelector(".title>a");
-  const rate = itemBody.querySelector(".rating_nums");
-  const desc = itemBody.querySelector(".abstract");
-  const comment = listItem.querySelector(".comment-item>.comment");
+const interpretDomNOutput = () => {
+  dom.window.document
+    .querySelectorAll(".doulist-item")
+    .forEach((listItem, i) => {
+      const itemBody = listItem.querySelector(".mod .bd.doulist-subject");
 
-  const textCollection = {
-    url: anchor?.href?.trim(),
-    name: anchor?.textContent?.trim(),
-    rate: rate?.textContent?.trim(),
-    abst: desc?.textContent?.trim().replace(/(\n +)+/g, "\n"),
-    location: comment?.textContent?.trim().replace("评语：", ""),
-  };
+      const anchor = itemBody.querySelector(".title>a");
+      const rate = itemBody.querySelector(".rating_nums");
+      const desc = itemBody.querySelector(".abstract");
+      const comment = listItem.querySelector(".comment-item>.comment");
 
-  /** 
-   * @type {[string, string][]}
-   * @example [ [ '10月12日', '百子湾厅' ], [ '10月30日', '2号厅' ] ]
-   */
-  const locEntries = textCollection.location.split("\n").map((entry) => {
-    return entry.split(" ");
-  });
+      const textCollection = {
+        name: anchor?.textContent?.trim(),
+        url: anchor?.href?.trim(),
+        rate: rate?.textContent?.trim(),
+        abst: desc?.textContent?.trim().replace(/(\n +)+/g, "\n"),
+        location: comment?.textContent?.trim().replace("评语：", ""),
+      };
 
-  const textArr = Object.values(textCollection);
+      /**
+       * @type {[string, string][]}
+       * @example [ [ '10月12日', '百子湾厅' ], [ '10月30日', '2号厅' ] ]
+       */
+      const dateLocEntries = textCollection.location
+        .split("\n")
+        .map((entry) => entry.split(" "));
 
-  if (textCollection.rate >= 8) {
-    console.log(
-      `影片${i + 1}:\n` +
-        textArr.join("\n") +
-        "\n--------------------------------"
-    );
-  }
-});
+      const textArr = Object.values(textCollection);
+
+      const text = `
+影片${i + 1}:
+${textArr.join("\n")}
+  
+--------------------------------
+`;
+
+      const goodOrBad = textCollection.rate >= 8 ? "good" : "bad";
+      appendFile(outputFilePath[goodOrBad], text);
+    });
+};
