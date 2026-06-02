dafd9265536aa7e927ca4c518328bc7f1e4fef72
Mingeax <wsyym@qq.com>
2026-06-02 10:09:36 +0800

    feat: 邮箱配置指导

--- auto-email/config.js
+++ auto-email/config.js
@@ -1,8 +1,10 @@
 export default {
   email: {
-    service: "",
-    user: "",
-    accessToken: "",
+    // 以qq邮箱为例
+    service: "", // 如 smtp.qq.com
+    user: "", // 如 xxx@qq.com
+    accessToken: "", // 授权码
+    // 以下字段留空
     title: "",
     content: {
       text: "",
